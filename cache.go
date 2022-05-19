6dacb180c096fc8ce08fb241e1911cefb9f0ce8d
Teh|Striker <kristianhanus@gmail.com>
2022-05-19 23:06:37 +0200

    Implement cache and cacher

--- cache.go
+++ cache.go
@@ -1,5 +1,11 @@
 package cache
 
+import (
+	"github.com/patrickmn/go-cache"
+	"strconv"
+	"time"
+)
+
 // Fetcher defines methods used to obtain data from provider.
 type Fetcher interface {
 	// Fetch obtains string representation of id from provider.
@@ -9,8 +15,38 @@ type Fetcher interface {
 	FetchAll() (map[int]string, error)
 }
 
-type Cache struct{}
+// Cacher defines methods used to obtain data from cache.
+type Cacher interface {
+
+	// Get returns id translated to string; boolean for cache hit/miss.
+	Get(id int) (string, bool)
+}
+
+type Cache struct {
+	fs Fetcher
+	c  *cache.Cache
+}
 
 func NewCache(fs Fetcher) *Cache {
-	panic("implement me")
+	return &Cache{
+		fs: fs,
+		c:  cache.New(5*time.Minute, 10*time.Minute),
+	}
+}
+
+func (r *Cache) Get(id int) (string, bool) {
+	convertedVal := strconv.Itoa(id)
+
+	cachedValue, found := r.c.Get(convertedVal)
+	if found {
+		return cachedValue.(string), true
+	} else {
+		value, err := r.fs.Fetch(id)
+		if err != nil {
+			return "", false
+		}
+
+		r.c.Set(convertedVal, value, cache.DefaultExpiration)
+		return value, true
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 go 1.18
 
 module gitlab.com/kiwicom/search-team/cache
+
+require github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
